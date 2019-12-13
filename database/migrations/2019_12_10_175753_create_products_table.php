c77173a5360ee1282e1e20aa138082cd88e9b087
unknown <hartoyy@hotmail.com>
2019-12-13 19:08:54 -0300

    cambio en migrations products table

--- database/migrations/2019_12_10_175753_create_products_table.php
+++ database/migrations/2019_12_10_175753_create_products_table.php
@@ -1,9 +1,9 @@
 <?php
- 
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-  
+
 class CreateProductsTable extends Migration
 {
     /**
@@ -14,15 +14,15 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('precio');
-            $table->text('detail');
+            $table->string('detail');
             $table->string('img');
             $table->timestamps();
         });
     }
-  
+
     /**
      * Reverse the migrations.
      *
