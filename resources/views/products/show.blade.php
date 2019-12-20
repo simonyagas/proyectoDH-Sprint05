2348c8b5ce62a9b4ee38b19f2248a89594fd7e07
simonyagas <simonyagas@gmail.com>
2019-12-20 18:46:39 -0300

    show nuevo

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2> Mostrar Producto</h2>
+                <h2> Mostrar Producto {{ $product->name }}</h2>
             </div>
             {{-- <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('products.index') }}"> Volver</a>
@@ -35,7 +35,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Imagen:</strong>                
-                <img src="{{ Storage::url($product->img)}}">
+                <img src="{{ Storage::url($product->img)}}" width="300">
                 
 
             </div>
